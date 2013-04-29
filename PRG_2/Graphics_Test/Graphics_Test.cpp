ae5bbc91af779f2efc4bbb0c0ceb593c1d493efe
Mattes Groeger <info@mattes-groeger.de>
2013-04-29 17:03:06 +0200

    Add asci output

--- PRG_2/Graphics_Test/Graphics_Test.cpp
+++ PRG_2/Graphics_Test/Graphics_Test.cpp
@@ -2,10 +2,31 @@
 //
 
 #include "stdafx.h"
+#include <string>
+#include <iostream>
 
+using namespace std;
 
-int _tmain(int argc, _TCHAR* argv[])
+void printChar(int index, char asci);
+
+int main(int argc, _TCHAR* argv[])
 {
+	// " " = 32
+	// voll = 219
+	// top = 223
+	// bottom = 220
+
+	for (int i = 32; i <= 254; i++)
+	{
+		printChar(i, i);
+	}
+
+	system("pause");
+
 	return 0;
 }
 
+void printChar(int index, char asci)
+{
+	cout << index << ":\t" << asci << endl;
+}
